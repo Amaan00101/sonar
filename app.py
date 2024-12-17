a81c62ade74b9133f190b25479d6374b7fe04c2f
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 <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>
2024-12-17 16:20:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+
+print ("This is my first Actiion")
+print ("This is my first Actiion")
+print ("This is my first Actiion")
+print ("This is my first Actiion")
+
+
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+print ("hello worlddd")
+
+print ("This is my first Actiion")
+print ("This is my first Actiion")
+print ("This is my first Actiion")
+print ("This is my first Actiion")
